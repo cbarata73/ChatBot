bf04e4475a8f83b1f02ca357734becc268e81c9d
Carlos Barata <cbarata73@gmail.com>
2025-07-03 11:11:50 +0100

    Mais interações

--- app.py
+++ app.py
@@ -29,7 +29,21 @@ def obter_resposta(texto: str) -> str:
         'tempo': 'Está um dia de sol!',
         ('bye', 'adeus', 'tchau'): 'Gostei de falar contigo! Até breve...',
         'historia de portugal': 'Portugal tem uma rica história...',
-    }
+        'cozinhar': 'Cozinhar é uma arte que envolve a preparação de alimentos...',
+        'sabes programar': 'Sim, posso ajudar com programação!',
+        'programar': 'Programar é o processo de escrever código para criar software...',
+        'desenvolvimento web': 'O desenvolvimento web envolve a criação de sites e aplicações web...',
+        'desenvolvimento de software': 'O desenvolvimento de software é o processo de criar programas e aplicações...',
+        'desenvolvimento de jogos': 'O desenvolvimento de jogos é a criação de jogos eletrônicos...',
+        'desenvolvimento de aplicativos móveis': 'O desenvolvimento de aplicativos móveis é a criação de aplicativos para dispositivos móveis...',
+        'ia': 'A inteligência artificial é um campo da ciência da computação que se concentra na criação de sistemas que podem realizar tarefas que normalmente requerem inteligência humana.',
+        'machine learning': 'O aprendizado de máquina é um subcampo da inteligência artificial que se concentra no desenvolvimento de algoritmos que permitem que os computadores aprendam com os dados.',
+        'deep learning': 'O aprendizado profundo é uma subárea do aprendizado de máquina que utiliza redes neurais profundas para modelar dados complexos.',
+        'saúde': 'A saúde é um estado de completo bem-estar físico, mental e social, e não apenas a ausência de doenças ou enfermidades.',
+        'problemas saúde': 'Problemas de saúde podem variar de leves a graves e podem afetar qualquer parte do corpo.',
+        ('indisposição', 'sintomas de indisposição', 'estou com sintomas de indisposição'): 'Sintomas de indisposição podem incluir fadiga, dor de cabeça, náusea e outros sinais de que algo não está bem.',
+        'sintomas': 'Sintomas são sinais ou indicações de uma condição médica ou doença.',
+        }
 
     for chave, resposta in respostas.items():
         if isinstance(chave, tuple):
